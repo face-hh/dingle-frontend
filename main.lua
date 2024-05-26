903b2199f28e26c14bd15f0eacfa7c74bbceac4d
face-hh <xxfacexc@gmail.com>
2024-05-26 20:51:05 +0300

    update with the new multiselector

--- main.lua
+++ main.lua
@@ -1,44 +1,9 @@
 local query = get("query")
 local cards = get("card", true);
 
-local links = {
-    ["1"] = get("result1link"),
-    ["2"] = get("result2link"),
-    ["3"] = get("result3link"),
-    ["4"] = get("result4link"),
-    ["5"] = get("result5link"),
-    ["6"] = get("result6link"),
-    ["7"] = get("result7link"),
-    ["8"] = get("result8link"),
-    ["9"] = get("result9link"),
-    ["10"] = get("result10link")
-}
-
-local descriptions = {
-    ["1"] = get("result1desc"),
-    ["2"] = get("result2desc"),
-    ["3"] = get("result3desc"),
-    ["4"] = get("result4desc"),
-    ["5"] = get("result5desc"),
-    ["6"] = get("result6desc"),
-    ["7"] = get("result7desc"),
-    ["8"] = get("result8desc"),
-    ["9"] = get("result9desc"),
-    ["10"] = get("result10desc")
-}
-
-local domains = {
-    ["1"] = get("result1domain"),
-    ["2"] = get("result2domain"),
-    ["3"] = get("result3domain"),
-    ["4"] = get("result4domain"),
-    ["5"] = get("result5domain"),
-    ["6"] = get("result6domain"),
-    ["7"] = get("result7domain"),
-    ["8"] = get("result8domain"),
-    ["9"] = get("result9domain"),
-    ["10"] = get("result10domain")
-}
+local links = get("link", true)
+local descriptions = get("desc", true)
+local domains = get("domain", true)
 
 local visible = false;
 
@@ -61,11 +26,9 @@ query.on_submit(function(content)
 	})
 
     for k, v in pairs(res) do
-        local i = tostring(k);
-
-        local link = links[i];
-        local desc = descriptions[i];
-        local domain = domains[i];
+        local link = links[k];
+        local desc = descriptions[k];
+        local domain = domains[k];
         
         local URL = percentage(v["rating"], -999, 2) .. "% | buss://" .. v["domain"];
 
