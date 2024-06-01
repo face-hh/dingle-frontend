094729fda388adeb3c27312e715a95df7e52e6cb
face-hh <xxfacexc@gmail.com>
2024-06-01 18:32:25 +0300

    new api

--- main.lua
+++ main.lua
@@ -8,6 +8,55 @@ local domains = get("domain", true)
 local visible = false;
 
 query.on_submit(function(content)
+    cards_thing()
+
+    local res = fetch({
+		url = "http://search.buss.lol/search?q=" .. content,
+		method = "GET",
+		headers = { ["Content-Type"] = "application/json" },
+	})
+    print(res)
+    for k, v in pairs(res) do
+        local link = links[k];
+        local desc = descriptions[k];
+        local domain = domains[k];
+        
+        local URL = string.format("%g",string.format("%.2f", v["score"])) .. "% | buss://" .. v["url"];
+
+        domain.set_content(URL)
+        link.set_content(v["title"])
+        link.set_href("buss://" .. v["url"])
+        desc.set_content(v["description"])
+    end
+end)
+
+get("luck").on_click(function()
+    cards_thing()
+
+    local res = fetch({
+		url = "http://search.buss.lol/random",
+		method = "GET",
+		headers = { ["Content-Type"] = "application/json" },
+	})
+
+    local link = links[1];
+    local desc = descriptions[1];
+    local domain = domains[1];
+    
+    local URL = "buss://" .. res["url"];
+    
+    domain.set_content(URL)
+    link.set_content(res["title"])
+    link.set_href(URL)
+
+    if res["description"] then
+        desc.set_content(res["description"])
+    else
+        desc.set_content("No description :|")
+    end
+end)
+
+function cards_thing()
     if not visible then
         print("turning shit visible....")
 
@@ -17,42 +66,4 @@ query.on_submit(function(content)
 
         visible = true
     end
-
-    local res = fetch({
-		url = "http://search.buss.lol/search",
-		method = "POST",
-		headers = { ["Content-Type"] = "application/json" },
-		body = '{ "query": "' .. content .. '" }',
-	})
-
-    for k, v in pairs(res) do
-        local link = links[k];
-        local desc = descriptions[k];
-        local domain = domains[k];
-        
-        local URL = percentage(v["rating"], -999, 2) .. "% | buss://" .. v["domain"];
-
-        domain.set_content(URL)
-        link.set_content(v["title"])
-        link.set_href("buss://" .. v["domain"])
-        desc.set_content(v["description"])
-    end
-end)
-
-function percentage(value, min, max)
-    if value < min then
-        value = min
-    elseif value > max then
-        value = max
-    end
-    
-    local percentage = ((value - min) / (max - min)) * 100
-    
-    if percentage < 0 then
-        percentage = 0
-    elseif percentage > 100 then
-        percentage = 100
-    end
-
-    return string.format("%.2f", percentage)
 end
